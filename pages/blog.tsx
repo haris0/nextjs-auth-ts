1568d2f087a3a4f82be548fc86491f63163b3c66
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-12-06 14:24:35 +0700

    implment Server-side Authentication

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,21 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
+import { getSession } from 'next-auth/react';
 
-const Blog: NextPage = () => (
+const Blog: NextPage<{ data: string }> = ({ data }) => (
   <h1>
-    Blog Page
+    Blog Page - {data}
   </h1>
 );
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+      data: session ? 'List of 100 Personalized blog' : 'List of free blog',
+    },
+  };
+};
 
 export default Blog;
