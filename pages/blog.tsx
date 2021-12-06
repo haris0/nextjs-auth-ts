63a2e99977f8624489e0f234740a0a3ff110db33
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-12-06 11:50:50 +0700

    add blog and dashboard page

---
+++ pages/blog.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+
+const Blog: NextPage = () => (
+  <h1>
+    Blog Page
+  </h1>
+);
+
+export default Blog;
---
+++ pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+
+const Dashboard: NextPage = () => (
+  <h1>
+    Dashboard Page
+  </h1>
+);
+
+export default Dashboard;
