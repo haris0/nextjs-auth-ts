ecdbb0ef4cad3a1015a237f63d1bfa6c79b4070e
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-12-06 11:47:08 +0700

    implement and add navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@styles/globals.scss';
+import '@components/navbar.scss';
 import type { AppProps } from 'next/app';
+import Navbar from '@components/navbar';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Navbar />
+    <Component {...pageProps} />
+  </>
+);
 
 export default MyApp;
